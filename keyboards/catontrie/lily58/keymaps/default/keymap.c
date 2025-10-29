67d9e2d475afa447093f7561e9bc9f25322e4de5
Quan Pham <shiroemon95@gmail.com>
2025-10-29 02:16:08 -0700

    Revert "revision"
    This reverts commit f7d8ae056be3d485ee8d9faa4bb942c6fc62cef2.

--- keyboards/catontrie/lily58/keymaps/default/keymap.c
+++ keyboards/catontrie/lily58/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LAlt |SPACE | /LOWER  /       \RAISE \  |ENTER | LEFT |RIGHT |
+ *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE | LEFT |RIGHT |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                          KC_LGUI,    KC_LALT, KC_SPC, LOWER,  RAISE,    KC_ENT,  KC_LEFT, KC_RIGHT
+                          KC_LGUI,    KC_LALT, LOWER, KC_SPC,  KC_ENT,   RAISE,   KC_LEFT, KC_RIGHT
 ),
 
 
